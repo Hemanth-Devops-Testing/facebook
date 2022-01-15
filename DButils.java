7bd0a4cd9a6300289ac27b499a406991dce82965
Hemanthsivaram <sivaram40133@gmail.com>
2022-01-15 19:22:30 +0530

    updated in dev branch

--- DButils.java
+++ DButils.java
@@ -1,2 +1,3 @@
 Hello Guys Git commands learning by Hemanth
-i am updating the content to practice 
+i am updating the content to practice
+i am updating the code for bz i am in dev branch and i shold n't switch to masterbrach without comit in this branch
