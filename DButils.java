773952e1d5e9ed9355a3c0e182faa5f53cf871f4
Hemanthsivaram <sivaram40133@gmail.com>
2022-01-16 10:05:30 +0530

    db utils modified and test.js created

--- DButils.java
+++ DButils.java
@@ -1,3 +1,4 @@
 Hello Guys Git commands learning by Hemanth
 i am updating the content to practice
 i am updating the code for bz i am in dev branch and i shold n't switch to masterbrach without comit in this branch
+updating to practice the stash command 
