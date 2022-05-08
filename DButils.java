bfcce2aadda5c504b5213d9a6487f7452b85eaa2
Hemanthsivaram <sivaram40133@gmail.com>
2022-05-08 13:50:20 +0530

    To know why it's failed

--- DButils.java
+++ DButils.java
@@ -1,4 +1,5 @@
 Hello Guys Git commands learning by Hemanth
 i am updating the content to practice
 i am updating the code for bz i am in dev branch and i shold n't switch to masterbrach without comit in this branch
-updating to practice the stash command 
+updating to practice the stash command
+i want to know why 2nd time failed 
